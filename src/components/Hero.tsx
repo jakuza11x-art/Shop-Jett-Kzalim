d97b708d86c1108514fda15e2092348c214fca8e
jakuza11x-art <jakuza11x@gmail.com>
2026-06-20 10:38:26 +0700

    Create Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,31 @@
+export default function Hero(){
+
+return(
+
+<section className="container">
+
+<h1>
+
+Jett Kzalim Hotwar Store
+
+</h1>
+
+<p>
+
+Kho file Premium
+
+</p>
+
+<br/>
+
+<button className="btn">
+
+Khám phá
+
+</button>
+
+</section>
+
+)
+
+}
