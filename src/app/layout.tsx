e39185569e10c0671258adb478fa41116c6986f2
jakuza11x-art <jakuza11x@gmail.com>
2026-06-20 10:29:45 +0700

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+export const metadata = {
+  title: "Jett Kzalim Hotwar Store",
+
+  description: "Kho file premium"
+}
+
+export default function RootLayout({
+  children
+}:{
+  children:React.ReactNode
+}){
+
+return(
+
+<html lang="vi">
+
+<body>
+
+{children}
+
+</body>
+
+</html>
+
+)
+
+}
